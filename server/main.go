30d3aea0a362bc1c3d91cd88b581492d8719a680
TJ <termainejenkins@gmail.com>
2023-12-14 22:14:23 -0500

    Separated concerns of weatherdashboard.html and weatherdashboard.ts

--- server/main.go
+++ server/main.go
@@ -1,3 +1,5 @@
+// server/main.go
+
 package main
 
 import (
@@ -30,20 +32,39 @@ func main() {
 	// Serve static files from the "static" directory
 	router.StaticFS("/static", http.Dir(staticDir))
 
+	// Define a route to fetch weather data
+	router.GET("/api/weather", func(c *gin.Context) {
+		// Replace "YOUR_API_KEY" with your OpenWeatherMap API key
+		apiKey := "YOUR_API_KEY"
+		weatherURL := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=CityName&appid=%s", apiKey)
+
+		// Make a GET request to the OpenWeatherMap API
+		response, err := http.Get(weatherURL)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch weather data"})
+			return
+		}
+		defer response.Body.Close()
+
+		// Check if the request was successful (status code 200)
+		if response.StatusCode != http.StatusOK {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch weather data"})
+			return
+		}
+
+		// Parse the JSON response
+		var weatherData map[string]interface{}
+		if err := response.JSON(&weatherData); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse weather data"})
+			return
+		}
+
+		c.JSON(http.StatusOK, weatherData)
+	})
+
 	// Run the server on the IPv4 loopback address and port 8080
 	err := router.Run("127.0.0.1:8080")
 	if err != nil {
 		fmt.Printf("Failed to start server: %v\n", err)
 	}
-
-	// Define a route to fetch weather data
-	router.GET("/api/weather", func(c *gin.Context) {
-		// Mock weather data for testing
-		weatherData := map[string]interface{}{
-			"temperature": 25,
-			"condition":   "Sunny",
-		}
-		c.JSON(http.StatusOK, weatherData)
-	})
-
 }
--- src/components/WeatherDashboard.ts
+++ src/components/WeatherDashboard.ts
@@ -1,19 +1,17 @@
-<!-- src/components/WeatherDashboard.ts -->
-<div x-data="{ weather: null }" x-init="fetchWeatherData">
-    <h1 x-text="weather ? `Temperature: ${weather.temperature}°C, Condition: ${weather.condition}` : 'Loading...'"></h1>
-
-    <script>
-        // Specify types for data and fetchWeatherData function
-        type WeatherData = { temperature: number; condition: string };
-
-        async function fetchWeatherData(this: { weather: WeatherData | null }) {
-            try {
-                const response = await fetch('/api/weather');
-                const data: WeatherData = await response.json();
-                this.weather = data;
-            } catch (error) {
-                console.error('Error fetching weather data:', error);
-            }
-        }
-    </script>
-</div>
+// src/components/WeatherDashboard.ts
+export type WeatherData = {
+    temperature: number;
+    condition: string;
+  };
+  
+  export async function fetchWeatherData(): Promise<WeatherData> {
+    try {
+      const response = await fetch('/api/weather');
+      const data: WeatherData = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+      throw error; // Re-throw the error to be handled elsewhere
+    }
+  }
+  
